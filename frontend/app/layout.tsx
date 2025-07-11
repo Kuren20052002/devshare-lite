6ba5f319d3c945fba8f3fd1aa4bb2b0dc1d7f172
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2025-07-11 21:29:40 +0700

    feat: Add Hero page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import { AuthProvider } from "@/app/context/AuthContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
